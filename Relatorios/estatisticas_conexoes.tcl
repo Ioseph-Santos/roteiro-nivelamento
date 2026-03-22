b2b70ef98b7bccc6746ee84cb1274f4a93a0032b
Ioseph <iosephcontato@gmail.com>
2026-03-22 16:46:07 -0300

    Estatisticas_conexoes versao 2, apaguei do diretorio principal do repositório e deixei apenas no diretorio /Relatorios

--- Relatorios/estatisticas_conexoes.tcl
+++ Relatorios/estatisticas_conexoes.tcl
@@ -1,31 +1,52 @@
-set arquivo "contador_netlist.v"
-# Os módulos são somador, contador e flipflop
+set arquivo "netlist.v"
 set fp [open $arquivo r]
-set contanet 0
+
 set nome_in {}
 set nome_out {}
 set nome_wire {}
+
 while {[gets $fp data] >= 0} {
-	set data [string trim $data]
-	
-	switch -glob -- $data {
-		"input *" {
-			set nome [lindex $data 1] 
-			lappend nome_in $nome
-		}		
-		"output *" {
-			set nome [lindex $data 2]
-			lappend nome_out $nome
-		}
-		"wire *" {
-			set nome [lindex $data 1]
-			lappend nome_wire $nome
-		}
-		
-	}
-	}
-	puts $nome_in
-	puts $nome_out
-	puts $nome_wire
-	close $fp
-	
+    set data [string trim $data]
+    
+    # Limpeza: remove caracteres que "colam" nos nomes
+    set data [string map {"," "" ";" "" "reg" ""} $data]
+    
+    switch -glob -- $data {
+        "input *" {
+            # Se a linha tem '[', o nome real é o 3º elemento (índice 2)
+            if {[regexp {\[.*\]} $data]} {
+                set nome [lindex $data 2]
+            } else {
+                set nome [lindex $data 1]
+            }
+            if {$nome != ""} { lappend nome_in $nome }
+        }        
+        "output *" {
+            if {[regexp {\[.*\]} $data]} {
+                set nome [lindex $data 2]
+            } else {
+                set nome [lindex $data 1]
+            }
+            if {$nome != ""} { lappend nome_out $nome }
+        }
+        "wire *" {
+            if {[regexp {\[.*\]} $data]} {
+                set nome [lindex $data 2]
+            } else {
+                set nome [lindex $data 1]
+            }
+            if {$nome != ""} { lappend nome_wire $nome }
+        }
+    }
+}
+close $fp
+
+
+puts "=== LISTA DE NETS (TAREFA 3) ==="
+puts "Inputs:  $nome_in"
+puts "Outputs: $nome_out"
+puts "Wires:   $nome_wire"
+
+
+set todas_as_nets [lsort -unique [concat $nome_in $nome_out $nome_wire]]
+puts "\nTotal de nets únicas encontradas: [llength $todas_as_nets]"
